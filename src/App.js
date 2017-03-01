903c5041ca1f1b2afde3585de6e8a02f28c2f246
Aleksey Belchenkov <u608110@gmail.com>
2017-03-01 14:09:55 +0300

    Fetching The JSON Data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
 import Header from './Components/Header';
 import About from './Components/About';
 import Contact from './Components/Contact';
@@ -10,7 +12,38 @@ import Testimonials from './Components/Testimonials';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    }
+  }
+
+    getResumeData() {
+        $.ajax({
+            url: 'http://localhost:3000/resumeData.json',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({
+                    resumeData: data
+                });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.log(err);
+                alert(err);
+            }
+        });
+    }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
   render() {
+    console.log(this.state.resumeData);
     return (
       <div className="App">
           <Header />
